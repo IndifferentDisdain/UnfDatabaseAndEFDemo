cf8a7f5dbb1cfaae42b35133f97af97bd1815d4c
Joseph Short <josephpshort@gmail.com>
2015-06-17 15:46:49 -0400

    Loading bugs from server now (not db or services yet).

--- UnfDatabaseAndEFDemoApp/Controllers/BugsController.cs
+++ UnfDatabaseAndEFDemoApp/Controllers/BugsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,87 @@ namespace UnfDatabaseAndEFDemoApp.Controllers
 {
     public class BugsController : Controller
     {
+        private static readonly IList<BugListItem> _bugs;
+
+        static BugsController()
+        {
+            _bugs = new List<BugListItem>
+            {
+                new BugListItem
+                {
+                    Id = 1,
+                    Title = "Sample Bug 1",
+                    Status = BugStatus.New
+                },
+                new BugListItem
+                {
+                    Id = 2,
+                    Title = "Sample Bug 2",
+                    Status = BugStatus.New
+                },
+                new BugListItem
+                {
+                    Id = 3,
+                    Title = "Sample Bug 3",
+                    Status = BugStatus.New
+                },
+                new BugListItem
+                {
+                    Id = 4,
+                    Title = "Sample Bug 4",
+                    Status = BugStatus.InProgress
+                },
+                new BugListItem
+                {
+                    Id = 5,
+                    Title = "Sample Bug 5",
+                    Status = BugStatus.InProgress
+                },
+                new BugListItem
+                {
+                    Id = 6,
+                    Title = "Sample Bug 6",
+                    Status = BugStatus.InProgress
+                },
+                new BugListItem
+                {
+                    Id = 7,
+                    Title = "Sample Bug 7",
+                    Status = BugStatus.Completed
+                },
+                new BugListItem
+                {
+                    Id = 8,
+                    Title = "Sample Bug 8",
+                    Status = BugStatus.Completed
+                },
+                new BugListItem
+                {
+                    Id = 9,
+                    Title = "Sample Bug 9",
+                    Status = BugStatus.Completed
+                }
+            };
+        }
+
         // GET: Bugs
         public ActionResult Index()
         {
-            return View();
+            return View(_bugs);
         }
+    }
+
+    public class BugListItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public BugStatus Status { get; set; }
+    }
+
+    public enum BugStatus
+    {
+        New,
+        InProgress,
+        Completed
     }
 }
