4cf7ff81987f0a10e67cf89344d032392d798bae
Joseph Short <josephpshort@gmail.com>
2015-06-18 18:04:55 -0400

    Database project creation

---
+++ UnfDatabaseAndEFDemoApp.SqlServer/Scripts/PostDeployment/SeedInitialBugs.sql
@@ -0,0 +1,14 @@
+﻿IF NOT EXISTS(SELECT 1 FROM Bug)
+BEGIN
+
+INSERT INTO Bug(Title, Status) VALUES ('First Sample Bug', 0)
+INSERT INTO Bug(Title, Status) VALUES ('Second Sample Bug', 0)
+INSERT INTO Bug(Title, Status) VALUES ('Third Sample Bug', 0)
+INSERT INTO Bug(Title, Status) VALUES ('Fourth Sample Bug', 1)
+INSERT INTO Bug(Title, Status) VALUES ('Fifth Sample Bug', 1)
+INSERT INTO Bug(Title, Status) VALUES ('Sixth Sample Bug', 1)
+INSERT INTO Bug(Title, Status) VALUES ('Seventh Sample Bug', 2)
+INSERT INTO Bug(Title, Status) VALUES ('Eighth Sample Bug', 2)
+INSERT INTO Bug(Title, Status) VALUES ('Ninth Sample Bug', 2)
+
+END
---
+++ UnfDatabaseAndEFDemoApp.SqlServer/dbo/Tables/Bug.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Bug] (
+    [Id]     INT           IDENTITY (1, 1) NOT NULL,
+    [Title]  VARCHAR (100) NOT NULL,
+    [Status] TINYINT       NOT NULL
+);
+
