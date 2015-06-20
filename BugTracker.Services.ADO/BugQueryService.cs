a91672ab4eb587d9f065eea3551d2198a42ac161
Joseph Short <josephpshort@gmail.com>
2015-06-20 00:53:42 -0400

    Load bugs via ADO.NET

--- BugTracker.Services.ADO/BugQueryService.cs
+++ BugTracker.Services.ADO/BugQueryService.cs
@@ -1,70 +1,48 @@
 ﻿using BugTracker.Domain;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 namespace BugTracker.Services.ADO
 {
-    public class BugQueryService : IBugQueryService
+    public class BugQueryService : ServiceBase, IBugQueryService
     {
         public async Task<IList<BugListItem>> GetBugsList()
         {
-            return new List<BugListItem>
+            using (var conn = new SqlConnection(ConnectionString))
             {
-                new BugListItem
+                using (var cmd = conn.CreateCommand())
                 {
-                    Id = 1,
-                    Title = "Sample Bug 1",
-                    Status = BugStatus.New
-                },
-                new BugListItem
-                {
-                    Id = 2,
-                    Title = "Sample Bug 2",
-                    Status = BugStatus.New
-                },
-                new BugListItem
-                {
-                    Id = 3,
-                    Title = "Sample Bug 3",
-                    Status = BugStatus.New
-                },
-                new BugListItem
-                {
-                    Id = 4,
-                    Title = "Sample Bug 4",
-                    Status = BugStatus.InProgress
-                },
-                new BugListItem
-                {
-                    Id = 5,
-                    Title = "Sample Bug 5",
-                    Status = BugStatus.InProgress
-                },
-                new BugListItem
-                {
-                    Id = 6,
-                    Title = "Sample Bug 6",
-                    Status = BugStatus.InProgress
-                },
-                new BugListItem
-                {
-                    Id = 7,
-                    Title = "Sample Bug 7",
-                    Status = BugStatus.Completed
-                },
-                new BugListItem
-                {
-                    Id = 8,
-                    Title = "Sample Bug 8",
-                    Status = BugStatus.Completed
-                },
-                new BugListItem
-                {
-                    Id = 9,
-                    Title = "Sample Bug 9",
-                    Status = BugStatus.Completed
+                    var retVal = new List<BugListItem>();
+
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "SELECT Id, Title, Status FROM Bug ORDER BY Id";
+
+                    try
+                    {
+                        conn.Open();
+                        using (var reader = await cmd.ExecuteReaderAsync())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                retVal.Add(new BugListItem
+                                {
+                                    Id = int.Parse(reader["Id"].ToString()),
+                                    Title = reader["Title"].ToString(),
+                                    Status = (BugStatus)Enum.Parse(typeof(BugStatus), reader["Status"].ToString())
+                                });
+                            }
+                            return retVal;
+                        }
+                    }
+                    catch
+                    {
+                        throw;
+                    }
                 }
-            };
+            }
         }
     }
 }
---
+++ BugTracker.Services.ADO/ServiceBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Services.ADO
+{
+    public class ServiceBase
+    {
+        private static readonly string _connectionString;
+
+        static ServiceBase()
+        {
+            _connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["appContext"].ConnectionString;
+        }
+
+        public string ConnectionString
+        {
+            get { return _connectionString; }
+        }
+    }
+}
